eed4968829964025a0bb75cf5c7377c6cf41dd72
LSC <78481036+LL-res@users.noreply.github.com>
2022-11-16 17:51:00 +0800

    +huawei feature

--- internal/providers/huawei/api.go
+++ internal/providers/huawei/api.go
@@ -2,13 +2,15 @@ package huawei
 
 import (
 	"context"
-	"github.com/galayx-future/costpilot/internal/constants/cloud"
-	"github.com/galayx-future/costpilot/internal/providers/types"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/galaxy-future/costpilot/internal/constants/cloud"
+	"github.com/galaxy-future/costpilot/internal/providers/types"
+	"github.com/galaxy-future/costpilot/tools/limiter"
 	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/auth/global"
 	bss "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/bss/v2"
 	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/bss/v2/model"
 	regionHuawei "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/bss/v2/region"
-
+	"strconv"
 )
 
 type HuaweiCloud struct {
@@ -33,64 +35,52 @@ func (*HuaweiCloud) ProviderType() cloud.Provider {
 	return cloud.HuaweiCloud
 }
 
-func (p *HuaweiCloud) QueryAccountBill(ctx context.Context, param types.QueryAccountBillRequest) (types.DataInQueryAccountBill, error) {
-	var err error
-	billItems := make([]types.AccountBillItem, 0)
-	request := &model.ListCustomerselfResourceRecordsRequest{}
-	request.Cycle = param.BillingCycle
-	offsetRequest := int32(0)
-	request.Offset = &offsetRequest
-	limitRequest := int32(10)
-	request.Limit = &limitRequest
-
-	pageNum := int32(0)
-	response := new(model.ListCustomerselfResourceRecordsResponse)
-	for {
-		response, err = p.bssClientOpt.ListCustomerselfResourceRecords(request)
-
-		if err != nil {
-			return types.DataInQueryAccountBill{}, err
-		}
-		totalCount := response.TotalCount
-		if len(billItems) == 0 {
-			billItems = make([]types.AccountBillItem, 0, *totalCount)
-		}
-		billItems = append(billItems, convQueryAccountBill(response, *response.Currency)...)
-		if len(billItems) >= int(*totalCount) {
-			break
-		}
-		pageNum++
-		request.Offset = &pageNum
+func (p *HuaweiCloud) QueryAccountBill(ctx context.Context, param types.QueryAccountBillRequest) (result types.DataInQueryAccountBill, err error) {
+	if param.Granularity == types.Daily {
+		result, err = p.queryAccountBillByDate(ctx, param)
 	}
-	result := types.DataInQueryAccountBill{
-		BillingCycle: *response.Currency,
-		AccountID:    "",
-		TotalCount:   len(billItems),
-		AccountName:  "",
-		Items: types.ItemsInQueryAccountBill{
-			Item: billItems,
-		},
+	if param.Granularity == types.Monthly {
+		result, err = p.queryAccountBillByMonth(ctx, param)
+	}
+	if err != nil {
+		return types.DataInQueryAccountBill{}, err
 	}
 
 	return result, nil
 }
-
-func convQueryAccountBill(response *model.ListCustomerselfResourceRecordsResponse, currency string) []types.AccountBillItem {
+func convQueryAccountBillByMonth(param types.QueryAccountBillRequest, response *model.ShowCustomerMonthlySumResponse) []types.AccountBillItem {
+	if response == nil || response.BillSums == nil {
+		return []types.AccountBillItem{}
+	}
+	result := make([]types.AccountBillItem, 0, len(*response.BillSums))
+	for _, v := range *response.BillSums {
+		temp := types.AccountBillItem{
+			PipCode:          convPipCode(tea.StringValue(v.ServiceTypeCode)),
+			ProductName:      tea.StringValue(v.ServiceTypeName),
+			SubscriptionType: convSubscriptionType(strconv.Itoa(int(tea.Int32Value(v.ChargingMode)))),
+			Currency:         tea.StringValue(response.Currency),
+			PretaxAmount:     tea.Float64Value(v.CashAmount),
+		}
+		result = append(result, temp)
+	}
+	return result
+}
+func convQueryAccountBill(response *model.ListCustomerselfResourceRecordsResponse) []types.AccountBillItem {
 	if response == nil {
-		return nil
+		return []types.AccountBillItem{}
 	}
 
 	feeRecords := *response.FeeRecords
 	result := make([]types.AccountBillItem, 0, len(feeRecords))
 	for _, v := range feeRecords {
-		standardPipCode := convPipCode(*v.CloudServiceType)
+		standardPipCode := convPipCode(tea.StringValue(v.CloudServiceType))
 		item := types.AccountBillItem{
 			PipCode:          standardPipCode,
-			ProductName:      *v.ProductName,
-			BillingDate:      *v.BillDate, // has date when Granularity=DAILY
+			ProductName:      tea.StringValue(v.CloudServiceTypeName),
+			BillingDate:      tea.StringValue(v.BillDate),
 			SubscriptionType: convSubscriptionType(*v.ChargeMode),
-			Currency:         currency,
-			PretaxAmount:     *v.Amount,
+			Currency:         tea.StringValue(response.Currency),
+			PretaxAmount:     tea.Float64Value(v.Amount),
 		}
 		result = append(result, item)
 	}
@@ -149,19 +139,165 @@ func (p *HuaweiCloud) DescribeMetricList(ctx context.Context, param types.Descri
 
 func (p *HuaweiCloud) DescribeInstanceAttribute(ctx context.Context, param types.DescribeInstanceAttributeRequest) (types.DescribeInstanceAttribute, error) {
 	// TODO implement me
-	panic("implement me")
+	return types.DescribeInstanceAttribute{}, nil
 }
 
 func (p *HuaweiCloud) DescribeRegions(ctx context.Context, param types.DescribeRegionsRequest) (types.DescribeRegions, error) {
-	panic("implement me")
+	return types.DescribeRegions{}, nil
 }
 
 func (p *HuaweiCloud) DescribeInstanceBill(ctx context.Context, param types.DescribeInstanceBillRequest, isAll bool) (types.DescribeInstanceBill, error) {
 	// TODO implement me
-	panic("implement me")
+	return types.DescribeInstanceBill{}, nil
 }
 
 func (p *HuaweiCloud) QueryAvailableInstances(ctx context.Context, param types.QueryAvailableInstancesRequest) (types.QueryAvailableInstances, error) {
 	// TODO implement me
-	panic("implement me")
+	return types.QueryAvailableInstances{}, nil
+}
+
+func (p *HuaweiCloud) queryAccountBillByMonth(ctx context.Context, param types.QueryAccountBillRequest) (result types.DataInQueryAccountBill, err error) {
+
+	billItems := make([]types.AccountBillItem, 0)
+	request := &model.ShowCustomerMonthlySumRequest{}
+	request.BillCycle = param.BillingCycle
+	pageNum := int32(0)
+	request.Offset = tea.Int32(0)
+	request.Limit = tea.Int32(10)
+	response := new(model.ShowCustomerMonthlySumResponse)
+	if param.IsGroupByProduct {
+		for {
+			limiter := limiter.Limiters.GetLimiter(p.ProviderType().String()+"-"+"ShowCustomerMonthlySum", 9)
+			limiter.Take()
+			response, err = p.bssClientOpt.ShowCustomerMonthlySum(request)
+
+			if err != nil {
+				return types.DataInQueryAccountBill{}, err
+			}
+			totalCount := response.TotalCount
+			if len(billItems) == 0 {
+				billItems = make([]types.AccountBillItem, 0, *totalCount)
+			}
+			billItems = append(billItems, convQueryAccountBillByMonth(param, response)...)
+			if len(billItems) >= int(*totalCount) {
+				break
+			}
+			pageNum += *request.Limit
+			request.Offset = &pageNum
+		}
+		result = types.DataInQueryAccountBill{
+			BillingCycle: param.BillingCycle,
+			TotalCount:   int(tea.Int32Value(response.TotalCount)),
+			Items:        types.ItemsInQueryAccountBill{Item: billItems},
+		}
+	} else {
+		limiter := limiter.Limiters.GetLimiter(p.ProviderType().String()+"-"+"ShowCustomerMonthlySum", 9)
+		limiter.Take()
+		response, err = p.bssClientOpt.ShowCustomerMonthlySum(request)
+		if err != nil {
+			return types.DataInQueryAccountBill{}, err
+		}
+		result = types.DataInQueryAccountBill{
+			BillingCycle: param.BillingCycle,
+			TotalCount:   int(tea.Int32Value(response.TotalCount)),
+			Items: types.ItemsInQueryAccountBill{
+				Item: []types.AccountBillItem{
+					{
+						Currency:     tea.StringValue(response.Currency),
+						PretaxAmount: tea.Float64Value(response.CashAmount),
+					},
+				},
+			},
+		}
+	}
+
+	return result, nil
+}
+func (p *HuaweiCloud) queryAccountBillByDate(ctx context.Context, param types.QueryAccountBillRequest) (result types.DataInQueryAccountBill, err error) {
+	billItems := make([]types.AccountBillItem, 0)
+	request := &model.ListCustomerselfResourceRecordsRequest{}
+	request.Cycle = param.BillingCycle
+	request.BillDateBegin = tea.String(param.BillingDate)
+	request.BillDateEnd = tea.String(param.BillingDate)
+	includeZeroRecordRequest := false
+	request.IncludeZeroRecord = &includeZeroRecordRequest
+	request.Offset = tea.Int32(0)
+	request.Limit = tea.Int32(10)
+	pageNum := int32(0)
+	response := new(model.ListCustomerselfResourceRecordsResponse)
+	for {
+		limiter := limiter.Limiters.GetLimiter(p.ProviderType().String()+"-"+"ListCustomerselfResourceRecords", 9)
+		limiter.Take()
+		response, err = p.bssClientOpt.ListCustomerselfResourceRecords(request)
+
+		if err != nil {
+			return types.DataInQueryAccountBill{}, err
+		}
+		totalCount := response.TotalCount
+		if len(billItems) == 0 {
+			billItems = make([]types.AccountBillItem, 0, *totalCount)
+		}
+		billItems = append(billItems, convQueryAccountBill(response)...)
+		if len(billItems) >= int(*totalCount) {
+			break
+		}
+		pageNum += tea.Int32Value(request.Limit)
+		request.Offset = &pageNum
+	}
+	tempMap := make(map[string]*types.AccountBillItem, len(billItems))
+	if param.IsGroupByProduct {
+		for _, v := range billItems {
+			if val, ok := tempMap[v.ProductName+v.SubscriptionType.String()]; ok {
+				val.PretaxAmount += v.PretaxAmount
+			} else {
+				tempMap[v.ProductName+v.SubscriptionType.String()] = &types.AccountBillItem{
+					PipCode:          v.PipCode,
+					ProductName:      v.ProductName,
+					BillingDate:      v.BillingDate,
+					SubscriptionType: v.SubscriptionType,
+					Currency:         v.Currency,
+					PretaxAmount:     v.PretaxAmount,
+				}
+			}
+		}
+	} else {
+		var totalCost float64
+		var currency string
+		for _, v := range billItems {
+			totalCost += v.PretaxAmount
+			if currency == "" {
+				currency = v.Currency
+			}
+		}
+		return types.DataInQueryAccountBill{
+			BillingCycle: param.BillingCycle,
+			AccountID:    "",
+			TotalCount:   1,
+			AccountName:  "",
+			Items: types.ItemsInQueryAccountBill{
+				Item: []types.AccountBillItem{
+					{
+						BillingDate:  param.BillingDate,
+						Currency:     currency,
+						PretaxAmount: totalCost,
+					},
+				},
+			},
+		}, nil
+	}
+	resultItems := make([]types.AccountBillItem, 0, len(tempMap))
+	for _, v := range tempMap {
+		resultItems = append(resultItems, *v)
+	}
+	result = types.DataInQueryAccountBill{
+		BillingCycle: param.BillingCycle,
+		AccountID:    "",
+		TotalCount:   len(resultItems),
+		AccountName:  "",
+		Items: types.ItemsInQueryAccountBill{
+			Item: resultItems,
+		},
+	}
+
+	return result, nil
 }
