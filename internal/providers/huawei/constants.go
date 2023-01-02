8051fc84fe9c2090e827033279cc83d8ba869eba
baiyutang <irichardwang@gmail.com>
2023-01-02 15:47:15 +0800

    chore(huaweicloud):DescribeMetricList

---
+++ internal/providers/huawei/constants.go
@@ -0,0 +1,11 @@
+package huawei
+
+const (
+	_bssRegion       = "cn-north-1"
+	_chargingMode    = "charging_mode"
+	_floating        = "floating"
+	_instanceId      = "instance_id"
+	_average         = "average"
+	_namespaceAGTECS = "AGT.ECS"
+	_maxMetricsNum   = 500
+)
